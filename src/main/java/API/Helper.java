7aa0d479a6942084ffe3eb35bccb29f9039dda13
Izat <kerimbaevazat@gmail.com>
2023-06-05 09:34:49 +0600

    Delete some classes

--- src/main/java/API/Helper.java
+++
@@ -1,8 +0,0 @@
-package API;
-
-public class Helper {
-    public static int sum(int a,int b){
-       return a+b;
-
-    }
-}
--- src/main/java/API/endpoint/UserEndpoint.java
+++
@@ -1,5 +0,0 @@
-package API.endpoint;
-
-public class UserEndpoint {
-    private static final String USERS="users";
-}
